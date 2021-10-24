54bcd089dbedc7dc11ccfa592d560c3dac025f81
hpteach2021 <hooper.rob@gmail.com>
2021-10-24 12:58:23 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useRef } from "react";
+import "../src/styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./util";
+import Library from "./components/Library";
+import Navigation from "./components/Navigation";
+
+function App() {
+  const [songs, setSongs] = useState(data());
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
+  
+  return (
+    <div className="App">
+      <Navigation 
+      setLibraryStatus={setLibraryStatus}
+      libraryStatus={libraryStatus}
+      />
+      <Song isPlaying={isPlaying} currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        songInfo={songInfo}
+        setSongInfo={setSongInfo}
+        songs={songs}
+        setSongs={songs}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+      />
+      <Library
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        setLibraryStatus={setLibraryStatus}
+        libraryStatus={libraryStatus}
+      />
+
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LibrarySong.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const LibrarySong = ({ isPlaying, song, songs, setCurrentSong, id, audioRef, setSongs }) => {
+  const songSelectHandler = () => {
+    const newSongs = songs.map((song)=>{
+      if(song.id === id){
+        return {
+          ...song,
+          active: true,
+        }
+          
+        } else {
+          return {
+            ...song,
+            active: false,
+      }
+    }
+    });
+    setSongs(newSongs)
+    setCurrentSong(song);
+    if(isPlaying) {
+      const playPromise = audioRef.current.play();
+      if(playPromise !== undefined){
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        })
+      }
+    }
+  };
+  return (
+    <div className={`library-song ${song.active ? 'selected' : ""}`} onClick={songSelectHandler} >
+      <img src={song.cover} alt={song.name} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
