43ccfd9e3df5c84c76483dc1084ce52e746e662c
hpteach2021 <hooper.rob@gmail.com>
2021-10-26 15:07:03 +0100

    added functions for Karan

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   };
   
   return (
-    <div className={`App ${libraryStatus ? 'library-active' : ''}`}>
+    
       <Navigation 
       setLibraryStatus={setLibraryStatus}
       libraryStatus={libraryStatus}
